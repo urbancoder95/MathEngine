c470630b5b745b5b9173d3bb877f4e3ed4842dc8
Sunnidhya <sunnidhya.roy@gmail.com>
2020-10-20 21:08:32 +0530

    Changes to shape

--- graph/tensor.py
+++ graph/tensor.py
@@ -101,6 +101,74 @@ class Tensor(DataNode):
                             "\'int\' or \'float\' not \'" +
                             type(self.__data).__name__ + "\'")
 
+    def __shapenew__(self):
+        """
+        METHOD DOC PENDING.
+
+        Scalar values will have the shape of ().
+        Vector values will have shapes of (n,).
+        Matrices will have shapes of (m, n)
+        . . .
+        .
+        .
+        """
+        # Initially check for scalar values.
+        # To do this we check the length of the whole value.
+        # If it throws a TypeError
+        # (which is because it is not a list), then it's a list.
+        try:
+            length = len(self.__data)
+            self.__findList__(self.__data)
+            print("No Dimension mismatch error")
+        except TypeError:
+            length = 0
+        # Now we check if value is None, which means it is empty. None itself is a scalar value.
+        if self.__data is None:
+            return ()
+        # We check weather the value is not None but still scalar of types int or float.
+        elif length == 0 and (type(self.__data) == int or type(self.__data) == float):
+            return ()
+        # Then we check for the entire shape of the value
+        elif len(self.__data) > 0:
+            length = len(self.__data)
+            shape = list()
+            index = 0
+            next_val = self.__data
+            while length > 0:
+                # Keep on adding values to the shape
+                # attribute until a scalar is encountered
+                shape.append(length)
+                length = self.__check_len__(next_val)
+                next_val = next_val[index]
+            return tuple(shape)
+        else:
+            raise TypeError("Can only work with values of type " +
+                            "\'int\' or \'float\' not \'" +
+                            type(self.__data).__name__ + "\'")
+
+    def __findList__(self, listVal):
+        print(listVal)
+        flag, tempList = self.__checkLengthOfElements__(listVal)
+        if (len(tempList) == 1 and tempList[0] <= 1):
+            return
+        if (flag):
+            for elem in listVal:
+                self.__findList__(elem)
+                return
+        else:
+            raise Exception("Dimension mismatch of vector components" + listVal)
+
+    def __checkLengthOfElements__(self,value):
+        temp = []
+        for item in value:
+            try:
+                val = len(item)
+                temp.append(val)
+            except:
+                temp.append(1)
+        print(temp)
+        return all(i == item[0] for i in temp), temp
+
     def __check_len__(self, next_val):
         # Checks the length of the inner elements of next_val.
         # NEEDS IMPROVEMENT AS IT DOES NOT NEST FOR ALL THE INNER VALUES
